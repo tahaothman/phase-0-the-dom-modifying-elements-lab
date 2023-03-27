42116cf3ec83de2cfe9fcf82db96f5c8463ee4a1
Taha <taha.othman@outlook.com>
2023-03-27 17:10:23 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Taha is the champion";
+document.body.append(newHeader);
+
